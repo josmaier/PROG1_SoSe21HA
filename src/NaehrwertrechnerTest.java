79f38666c56298a4cfc4267d83bb865ff3411319
josch <joscha.maier@live.de>
2021-05-18 03:16:24 +0200

    Added test function for methods from class Person

--- src/NaehrwertrechnerTest.java
+++ src/NaehrwertrechnerTest.java
@@ -1,24 +1,58 @@
 public class NaehrwertrechnerTest {
-    public static void main(String[] args){
+    /**
+     * this program tests mehtods from the classes Lebensmittel and Person
+     */
+    public static void main(String[] args) {
 
-        //array to save new Nahrungsmittel
-        Nahrungsmittel[] Lebensmittel = new Nahrungsmittel[5];
+        /*
+          Array for saving objects of class Lebensmittel
+         */
+        Nahrungsmittel[] lebensmittel = new Nahrungsmittel[5];
 
-        //filling array with new Nahrungsmittel using the constructor
-        Lebensmittel[0] = new Nahrungsmittel("Nutella", 539, 30.9, 56.3, 0.107);
+        /*
+         Creating objects of class Lebensmittel using the constructor
+         */
+        lebensmittel[0] = new Nahrungsmittel("Nutella", 539, 30.9, 56.3, 0.107);
+        lebensmittel[1] = new Nahrungsmittel("Kiwi", 62, 0.6, 9, 0);
+        lebensmittel[2] = new Nahrungsmittel("Choco Fresh", 577, 42.7, 39.6, 0.153);
+        lebensmittel[3] = new Nahrungsmittel("Lachs Filet", 195, 9.4, 0, 0);
+        lebensmittel[4] = new Nahrungsmittel("Goldbären", 343, 0.5, 46, 0.07);
 
-        Lebensmittel[1] = new Nahrungsmittel("Kiwi", 62, 0.6, 9, 0);
-
-        Lebensmittel[2] = new Nahrungsmittel("Choco Fresh", 577, 42.7, 39.6, 0.153);
-
-        Lebensmittel[3] = new Nahrungsmittel("Lachs Filet", 195, 9.4, 0, 0);
-
-        Lebensmittel[4] = new Nahrungsmittel("Goldbären", 343, 0.5, 46, 0.07);
-
-        //outputting info for all array elements
-        for (Nahrungsmittel nahrungsmittel : Lebensmittel) {
+        /*
+        Outputting String with information for each object
+         */
+        for (Nahrungsmittel nahrungsmittel : lebensmittel) {
             nahrungsmittel.info();
         }
 
+        /*
+        inserting empty line for readability
+         */
+        System.out.println();
+
+        /*
+          Array for saving objects of class Person
+         */
+        Person[] personen = new Person[2];
+
+        /*
+         Creating objects of class Person using the constructor
+         */
+        personen[0] = new Person("Stefan");
+        personen[1] = new Person("Etlam");
+
+        /*
+        Testing consume method from class Person
+         */
+        personen[0].konsumiere(lebensmittel[1], 1500);
+        personen[0].konsumiere(lebensmittel[0], 300);
+        personen[1].konsumiere(lebensmittel[4], 1000);
+        personen[1].konsumiere(lebensmittel[2], 50);
+
+        /*
+        Outputting String with information about each object
+         */
+        personen[0].info();
+        personen[1].info();
     }
 }
